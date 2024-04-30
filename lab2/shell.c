8e6a634d2e31573a6ec2a9e78667e62cc7440916
phcool <2238659469@qq.com>
2024-04-30 13:30:10 +0800

    end

--- lab2/shell.c
+++ lab2/shell.c
@@ -20,23 +20,8 @@
 pid_t background_processes[128];
 int background_processes_count = 0;
 
-/*
- * 需要大家完成的代码已经用注释`DO:`标记
- * 可以编辑器搜索找到
- * 使用支持TODO高亮编辑器（如vscode装TODO highlight插件）的同学可以轻松找到要添加内容的地方。
- */
 
-/*
-    int split_string(char* string, char *sep, char** string_clips);
-
-    基于分隔符sep对于string做分割，并去掉头尾的空格
-
-    arguments:      char* string, 输入, 待分割的字符串
-                    char* sep, 输入, 分割符
-                    char** string_clips, 输出, 分割好的字符串数组
-
-    return:   分割的段数
-*/
+//基于分隔符sep对于string做分割，并去掉头尾的空格
 void add_background_process(pid_t pid);
 
 int split_string(char *string, char *sep, char **string_clips)
@@ -62,23 +47,13 @@ int split_string(char *string, char *sep, char **string_clips)
     return clip_num;
 }
 
-/*
-    执行内置命令
-    arguments:
-        argc: 输入，命令的参数个数
-        argv: 输入，依次代表每个参数，注意第一个参数就是要执行的命令，
-        若执行"ls a b c"命令，则argc=4, argv={"ls", "a", "b", "c"}
-        fd: 输出，命令输入和输出的文件描述符 (Deprecated)
-    return:
-        int, 若执行成功返回0，否则返回值非零
-*/
+//执行内置命令
 int exec_builtin(int argc, char **argv, int *fd)
 {
     if (argc == 0)
     {
         return 0;
     }
-    /* DO: 添加和实现内置指令 */
 
     if (strcmp(argv[0], "cd") == 0)
     {
@@ -114,14 +89,11 @@ int exec_builtin(int argc, char **argv, int *fd)
         else
         {
             // 没有指定 PID，将最近启动的进程移到前台
-            // 此处需要实现找到最近启动的进程的逻辑
-            // 假设最近启动的进程的 PID 是 123，这里就写死了
             pid =background_processes[background_processes_count-1];
         }
         // 将指定 PID 的进程移到前台
         if (pid > 0)
         {
-            // 可以使用 kill 函数发送 SIGCONT 信号将进程移到前台
             kill(pid, SIGCONT);
             // 等待进程结束
             waitpid(pid, NULL, 0);
@@ -138,14 +110,11 @@ int exec_builtin(int argc, char **argv, int *fd)
         else
         {
             // 没有指定 PID，恢复最近启动的进程
-            // 此处需要实现找到最近启动的进程的逻辑
-            // 假设最近启动的进程的 PID 是 123，这里就写死了
             pid =background_processes[background_processes_count-1];
         }
         // 恢复指定 PID 的进程
         if (pid > 0)
         {
-            // 可以使用 kill 函数发送 SIGCONT 信号将进程恢复到后台
             kill(pid, SIGCONT);
         }
     }
@@ -160,15 +129,7 @@ int exec_builtin(int argc, char **argv, int *fd)
 /*
     从argv中删除重定向符和随后的参数，并打开对应的文件，将文件描述符放在fd数组中。
     运行后，fd[0]读端的文件描述符，fd[1]是写端的文件描述符
-    arguments:
-        argc: 输入，命令的参数个数
-        argv: 输入，依次代表每个参数，注意第一个参数就是要执行的命令，
-        若执行"ls a b c"命令，则argc=4, argv={"ls", "a", "b", "c"}
-        fd: 输出，命令输入和输出使用的文件描述符
-    return:
-        int, 返回处理过重定向后命令的参数个数
 */
-
 int process_redirect(int argc, char **argv, int *fd)
 {
     /* 默认输入输出到命令行，即输入STDIN_FILENO，输出STDOUT_FILENO */
@@ -180,7 +141,6 @@ int process_redirect(int argc, char **argv, int *fd)
         int tfd;
         if (strcmp(argv[i], ">") == 0)
         {
-            // DO: 打开输出文件从头写入
             tfd = open(argv[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0666);
             if (tfd < 0)
             {
@@ -188,14 +148,12 @@ int process_redirect(int argc, char **argv, int *fd)
             }
             else
             {
-                // DO: 输出重定向
                 fd[WRITE_END] = tfd;
             }
             i += 2;
         }
         else if (strcmp(argv[i], ">>") == 0)
         {
-            // DO: 打开输出文件追加写入
             tfd = open(argv[i + 1], O_WRONLY | O_CREAT | O_APPEND, 0666);
             if (tfd < 0)
             {
@@ -203,14 +161,12 @@ int process_redirect(int argc, char **argv, int *fd)
             }
             else
             {
-                // DO:输出重定向
                 fd[WRITE_END] = tfd;
             }
             i += 2;
         }
         else if (strcmp(argv[i], "<") == 0)
         {
-            // DO: 读输入文件
             tfd = open(argv[i + 1], O_RDONLY, 0666);
             if (tfd < 0)
             {
@@ -218,7 +174,6 @@ int process_redirect(int argc, char **argv, int *fd)
             }
             else
             {
-                // DO:输出重定向
                 fd[READ_END] = tfd;
             }
             i += 2;
@@ -234,12 +189,6 @@ int process_redirect(int argc, char **argv, int *fd)
 
 /*
     在本进程中执行，且执行完毕后结束进程。
-    arguments:
-        argc: 命令的参数个数
-        argv: 依次代表每个参数，注意第一个参数就是要执行的命令，
-        若执行"ls a b c"命令，则argc=4, argv={"ls", "a", "b", "c"}
-    return:
-        int, 若执行成功则不会返回（进程直接结束），否则返回非零
 */
 int execute(int argc, char **argv)
 {
@@ -247,7 +196,7 @@ int execute(int argc, char **argv)
     // 默认输入输出到命令行，即输入STDIN_FILENO，输出STDOUT_FILENO
     fd[READ_END] = STDIN_FILENO;
     fd[WRITE_END] = STDOUT_FILENO;
-    // 处理重定向符，如果不做本部分内容，请注释掉process_redirect的调用
+    // 处理重定向符
     argc = process_redirect(argc, argv, fd);
     if (exec_builtin(argc, argv, fd) == 0)
     {
@@ -256,7 +205,6 @@ int execute(int argc, char **argv)
     // 将标准输入输出STDIN_FILENO和STDOUT_FILENO修改为fd对应的文件
     dup2(fd[READ_END], STDIN_FILENO);
     dup2(fd[WRITE_END], STDOUT_FILENO);
-    /* DO:运行命令与结束 */
 
     execvp(argv[0], argv);
     perror(argv[0]);
@@ -355,14 +303,12 @@ int main()
                 char *argv[MAX_CMD_ARG_NUM];
                 int argc;
                 int fd[2];
-                /* DO:处理参数，分出命令名和参数 */
                 /* 在没有管道时，内建命令直接在主进程中完成，外部命令通过创建子进程完成 */
                 argc = split_string(commands[0], " ", argv);
                 if (exec_builtin(argc, argv, fd) == 0)
                 {
                     continue;
                 }
-                /* DO:创建子进程，运行命令，等待命令运行结束*/
                 pid_t pid = fork();
                 if (pid == 0) // child
                 {
@@ -394,28 +340,20 @@ int main()
                 add_background_process(pid);
                 if (pid == 0)
                 {
-                    /*DO:子进程1 将标准输出重定向到管道，注意这里数组的下标被挖空了要补全*/
                     close(pipefd[READ_END]);                // 关闭读端
                     dup2(pipefd[WRITE_END], STDOUT_FILENO); // 重定向写端
                     close(pipefd[WRITE_END]);
-                    /*
-                        在使用管道时，为了可以并发运行，所以内建命令也在子进程中运行
-                        因此我们用了一个封装好的execute函数
-                     */
                     char *argv[MAX_CMD_ARG_NUM];
 
                     int argc = split_string(commands[0], " ", argv);
                     execute(argc, argv);
                     exit(255);
                 }
-                // 因为在shell的设计中，管道是并发执行的，所以我们不在每个子进程结束后才运行下一个
-                // 而是直接创建下一个子进程
                 // 子进程2
                 pid = fork();
                 add_background_process(pid);
                 if (pid == 0)
                 {
-                    /* DO:子进程2 将标准输入重定向到管道，注意这里数组的下标被挖空了要补全 */
                     close(pipefd[WRITE_END]);
                     dup2(pipefd[READ_END], STDIN_FILENO);
                     close(pipefd[READ_END]);
@@ -424,9 +362,6 @@ int main()
                     int argc = split_string(commands[1], " ", argv);
                     execute(argc, argv);
                     exit(255);
-                    /* DO:处理参数，分出命令名和参数，并使用execute运行
-                     * 在使用管道时，为了可以并发运行，所以内建命令也在子进程中运行
-                     * 因此我们用了一个封装好的execute函数*/
                 }
                 close(pipefd[WRITE_END]);
                 close(pipefd[READ_END]);
@@ -438,7 +373,6 @@ int main()
                 int read_fd; // 上一个管道的读端口（出口）
                 for (int i = 0; i < cmd_count; i++)
                 {
-                    /* DO:创建管道，n条命令只需要n-1个管道，所以有一次循环中是不用创建管道的 */
 
                     int pipefd[2];
                     if (i != cmd_count - 1)
@@ -455,17 +389,16 @@ int main()
                     add_background_process(pid);
                     if (pid == 0)
                     {
-                        /* DO:除了最后一条命令外，都将标准输出重定向到当前管道入口 */
+                        /* 除了最后一条命令外，都将标准输出重定向到当前管道入口 */
                         if (i != cmd_count - 1)
                             dup2(pipefd[WRITE_END], STDOUT_FILENO);
 
-                        /* DO:除了第一条命令外，都将标准输入重定向到上一个管道出口 */
+                        /* 除了第一条命令外，都将标准输入重定向到上一个管道出口 */
                         if (i != 0)
                             dup2(read_fd, STDIN_FILENO);
 
-                        /* DO:处理参数，分出命令名和参数，并使用execute运行
-                         * 在使用管道时，为了可以并发运行，所以内建命令也在子进程中运行
-                         * 因此我们用了一个封装好的execute函数 */
+                        /* 处理参数，分出命令名和参数，并使用execute运行
+                        */
                         char *argv[MAX_CMD_ARG_NUM];
                         int argc = split_string(commands[i], " ", argv);
                         execute(argc, argv);
@@ -473,13 +406,10 @@ int main()
                     }
                     /* 父进程除了第一条命令，都需要关闭当前命令用完的上一个管道读端口
                      * 父进程除了最后一条命令，都需要保存当前命令的管道读端口
-                     * 记得关闭父进程没用的管道写端口 */
+                      */
 
                     read_fd = pipefd[READ_END];
                     close(pipefd[WRITE_END]);
-
-                    // 因为在shell的设计中，管道是并发执行的，所以我们不在每个子进程结束后才运行下一个
-                    // 而是直接创建下一个子进程
                 }
                 while (wait(NULL) > 0)
                     ;
